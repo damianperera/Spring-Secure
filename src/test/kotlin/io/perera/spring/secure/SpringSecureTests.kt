07b70deda5529b1e7da684560b12b839f8211732
Damian Perera <damianperera@outlook.com>
2023-08-01 11:37:44 +0200

    tidied up tests

--- src/test/kotlin/io/perera/spring/secure/SpringSecureTests.kt
+++ src/test/kotlin/io/perera/spring/secure/SpringSecureTests.kt
@@ -1,25 +1,26 @@
 package io.perera.spring.secure
 
+import io.perera.spring.secure.domain.OWASPHeader
+import io.perera.spring.secure.service.ResponseInterceptor
 import io.perera.spring.secure.test.ContextAwareTest
 import org.junit.jupiter.api.Test
+import org.mockito.Mockito
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.get
+import org.springframework.web.client.RestTemplate
 
 @ContextAwareTest
 class SpringSecureTests @Autowired constructor(
     private val mockMvc: MockMvc
 ) {
     @Test
-    fun `test controller returns expected content`() {
+    fun `test controller returns expected OWASP headers and content`() {
         mockMvc.get("/hello")
             .andExpect { status { isOk() } }
             .andExpect { content { string("Hello World!") } }
-    }
-
-    @Test
-    fun `test controller returns expected OWASP headers`() {
-        mockMvc.get("/hello")
             .andExpect { header { string("Cache-Control", "no-store, max-age=0") } }
+            .andExpect { header { doesNotExist("Host-Header") } }
     }
 }
--- src/test/kotlin/io/perera/spring/secure/test/ContextAwareTest.kt
+++ src/test/kotlin/io/perera/spring/secure/test/ContextAwareTest.kt
@@ -3,9 +3,7 @@ package io.perera.spring.secure.test
 import io.perera.spring.secure.dummy.TestApplication
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.ContextConfiguration
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, properties = ["logging.level.root=DEBUG"])
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, properties = ["logging.level.root=DEBUG"], classes = [TestApplication::class])
 @AutoConfigureMockMvc
-@ContextConfiguration(classes = [TestApplication::class])
 annotation class ContextAwareTest
